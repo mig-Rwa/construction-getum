8fc2194641be3f04b37ace72529a24d53aca9f49
mig-Rwa <miguelmbabatunga32@gmail.com>
2025-04-27 22:08:55 +0300

    Update: Use new quarry-construction.jpg image in hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,10 +67,10 @@ export default function Home() {
               </a>
             </div>
           </div>
-          <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+          <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none xl:ml-32">
             <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
               <Image
-                src="/images/quarry-hero.jpg"
+                src="/images/quarry-construction.jpg"
                 alt="Quarry operations showcase"
                 width={2432}
                 height={1442}
